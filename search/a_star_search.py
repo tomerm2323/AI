53b25d2ac6b84fef1435908fcc675e7a1dfa242c
ShemiPeretz <shemi.p17@gmail.com>
2022-11-12 13:35:22 +0200

    A star impl'

--- search/a_star_search.py
+++ search/a_star_search.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class PriorityQueue(object):
     def __init__(self):
@@ -8,7 +9,7 @@ class PriorityQueue(object):
         return ' '.join([str(i) for i in self.queue])
 
     # for checking if the queue is empty
-    def isEmpty(self):
+    def empty(self):
         return len(self.queue) == 0
 
     # for inserting an element in the queue
@@ -17,7 +18,7 @@ class PriorityQueue(object):
 
     # for popping an element based on Priority
     def delete(self):
-        if self.isEmpty():
+        if self.empty():
             return
         try:
             min_val = float("inf")
@@ -35,6 +36,9 @@ class PriorityQueue(object):
             print()
             exit()
 
+    def remove_from_queue(self, index):
+        del self.queue[index]
+
 
 # The function initializes and returns open
 def init_open():
@@ -51,30 +55,55 @@ def get_best(open_list):
 
 # The function returns neighboring locations of s_location
 def get_neighbors(grid, s_location):
-    # neighbors = []
-    raise NotImplementedError
-    # return neighbors
+    neighbors = []
+    cur_x, cur_y = s_location
+    grid_size = grid.shape - np.array((1, 1))
+    opt_pos = [(cur_x + 1, cur_y),
+                   (cur_x + 1 , cur_y + 1),
+                   (cur_x + 1, cur_y -1 ),
+                   (cur_x, cur_y),
+                   (cur_x , cur_y + 1),
+                   (cur_x, cur_y -1),
+                   (cur_x - 1, cur_y),
+                   (cur_x - 1, cur_y + 1),
+                   (cur_x - 1, cur_y - 1)
+               ]
+    for pos in opt_pos:
+        x, y = pos
+        if x >= 0 and x <= grid_size[0] and y >= 0 and y <= grid_size[-1] and str(grid[x, y]) != '@':
+            neighbors.append(pos)
+    return neighbors
 
 # The function returns whether or not s_location is the goal location
 def is_goal(s_location, goal_location):
-    raise NotImplementedError
+    return s_location == goal_location
 
 # The function estimates the cost to get from s_location to goal_location
 def calculate_heuristic(s_location, goal_location):
-    raise NotImplementedError
+    distance = 0
+    for s_i, g_i in zip(s_location, goal_location):
+        distance += abs(s_i - g_i)
+    return distance
 
 # The function returns whether n_location should be generated (checks in open_list)
 # removes a node from open_list if needed 
 def check_for_duplicates_open(n_location, s, open_list):
-    raise NotImplementedError
-  
+    if n_location in open_list.queue:
+        n_loc_index = open_list.queue.index(n_location)
+        if s[2] + 1 < open_list.queue[n_loc_index][2]:
+            open_list.remove_from_queue(n_loc_index)
+            open_list.insert(n_location)
+            return False
+        return True
+    return False
   
 # The function returns whether n_location should be generated (checks in closed_list)
 # removes a node from closed_list if needed  
 def check_for_duplicates_close(n_location, s, closed_list):
-    raise NotImplementedError
-    
-    
+    if n_location in closed_list:
+        return True
+    return False
+
 # Locations are tuples of (x, y)
 def astar_search(grid, start_location, goal_location):
     # State = (f, h, g, x, y, s_prev) # f = g + h (For Priority Queue)
